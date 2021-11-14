02e2f10a5b9e89861f0a6d4ed937a39f4e282dd7
Mario Muniz <mario@mariomuniz.com>
2021-11-13 21:11:43 -0800

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    YOURBOT_API_KEY: process.env.YOURBOT_API_KEY,
+  },
+}
---
+++ src/pages/_app.js
@@ -0,0 +1,8 @@
+import '../../theme/dist/bootstrap.min.css'
+import '../../theme/dist/yourbot-bootstrap.min.css'
+
+function MyApp({Component, pageProps}) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
