90e90e3fb20eb10e354c7fc490d0a2bdfbbe22df
Mario Muniz <mario@mariomuniz.com>
2021-11-14 21:57:01 -0800

    Fix broken Auth component

--- src/modules/auth/components/auth/Auth.js
+++ src/modules/auth/components/auth/Auth.js
@@ -2,7 +2,7 @@ import {signIn, useSession} from 'next-auth/react'
 
 import {useEffect} from 'react'
 
-export default function Auth() {
+export default function Auth({children}) {
   const {data: session, status} = useSession()
   const isUser = !!session?.user
 
