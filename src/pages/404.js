cc56abe7028fd691b8834313ed3dad840489605e
Mario Muniz <mario@mariomuniz.com>
2021-11-15 00:57:41 -0800

    More bootstrap 5 fixes

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,8 @@ export default function Custom404() {
   return (
     <div className="d-flex flex-column align-items-center justify-content-center w-100 mw-auto min-vh-100 text-center">
       <div className="d-flex align-items-center">
-        <h1 className="mb-0 border-right pr-4">404</h1>
-        <span className="pl-4">This page could not be found.</span>
+        <h1 className="mb-0 border-right pe-4">404</h1>
+        <span className="ps-4">This page could not be found.</span>
       </div>
       <Button className="mt-4" href="/" size="lg" variant="primary">
         Go back home
--- src/pages/500.js
+++ src/pages/500.js
@@ -4,8 +4,8 @@ export default function Custom500() {
   return (
     <div className="d-flex flex-column align-items-center justify-content-center w-100 mw-auto min-vh-100 text-center">
       <div className="d-flex align-items-center">
-        <h1 className="mb-0 border-right pr-4">500</h1>
-        <span className="pl-4">Something broke...</span>
+        <h1 className="mb-0 border-right pe-4">500</h1>
+        <span className="ps-4">Something broke...</span>
       </div>
       <Button className="mt-4" href="/" size="lg" variant="primary">
         Go back home
